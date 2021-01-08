cf66fd45410fb760f9b7d51b8266e87005b95cec
Juan Raymundo Carrillo Jasso <raymundo>
2021-01-07 18:14:09 -0600

    Create "show" view and controller method

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,6 +2,7 @@ class TodosController < ApplicationController
   def new
     @todo = Todo.new
   end
+
   def create
     @todo = Todo.create todo_params
     if @todo.save
@@ -9,6 +10,10 @@ class TodosController < ApplicationController
     else
       render 'new'
     end    
+  end
+
+  def show
+    @todo = Todo.find params[:id]
   end
   private
     def todo_params
